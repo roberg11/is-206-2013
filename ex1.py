33a89e8109eeb73f700f79a87dbe828733127d43
Robert <r.gjengaar@gmail.com>
2013-09-12 16:58:00 +0200

    Changes to ex1.py and added ex2.py
    Rewrote ex1 and added ex2.py

--- ex1.py
+++ ex1.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 print "Hello World"
-print "Hello again"
-print "ILTT"
-print "TIS"
-print "Y!P æøå"
-print "I'dMRY'not'."
-print ' "said" DNTT.'
+#print "Hello again"
+#print "ILTT"
+#print "TIS"
+#print "Y!P æøå"
+#print "I'dMRY'not'."
+#print ' "said" DNTT.'
---
+++ ex2.py
@@ -0,0 +1,8 @@
+# A comment, TISYCRYP
+# Anything after the # is ignored
+
+print "I could have code like this."
+
+# You can also use a comment to "disable" or comment out a piece of code
+
+print "This will run."
